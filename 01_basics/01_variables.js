c80435a0d5c2c419485d3ed25e6acbe6688d86b0
Nelsan Mandela <nelsanmandela123@gmail.com>
2026-05-25 15:35:43 +0000

    file name update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const myId=225;
+let myEmail="nelsan123@gmail.com"
+var myPassword="12345"
+
+myCity="New Delhi"
+let myState;
+
+// myId=555  const cant be changed
+
+// prefer do not use var bcz there is issue in block scope and functional scope
+
+myEmail="me@gmail.com"
+myPassword="821101"
+
+console.log(myId);
+console.log(myEmail);
+console.log(myPassword);
+console.log(myCity);
+console.log(myState);
+
+//instead of all console log we can use
+
+console.table([myId,myEmail,myPassword,myCity,myState])
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all js code in newer version
+
+// alert (3+3) // we are using node js not browser
+
+// console.log(3+3)
+
+// console.log("hello world!")
+
+// Data Types - (1) Primitive Data Types
+
+let age=25
+
+let name="nelsan"
+
+let isLoggedIn=true
+
+let temperature=null
+
+let state;
+
+let uniqueId = Symbol("id")
+
+// number => 2 to the power 53
+// bigint =>
+// string= ""
+// boolean=>true/false
+// null=> standalone value
+// undefined => value not assigned
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// object
+
+
+
