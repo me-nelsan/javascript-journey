ceab3a650e8433418127111af80cf4359e31f21e
Nelsan Mandela <nelsanmandela123@gmail.com>
2026-05-25 18:18:05 +0000

    conversion update

---
+++ 01_basics/03_conversion & operation.js
@@ -0,0 +1,61 @@
+let age="25abc"
+
+// console.log(typeof age);
+// console.log(typeof (age))
+
+// number conversion
+
+let numberValue=Number(age)
+//Number me convert ho jayega
+
+// console.log(typeof numberValue);
+// console.log(numberValue);
+
+// "25" =>	25	
+// "25abc" =>	NaN	
+// true =>	1	
+// false =>	0	
+// null => 0	
+// undefined =>	NaN
+// "" => 0
+// "  " => 0
+
+//Boolean conversion
+
+let userLoggedIn=5
+
+let booleanValue=Boolean(userLoggedIn)
+//Boolean me convert ho jayega
+
+// console.log(typeof booleanValue)
+// console.log(booleanValue)
+
+// 0 =>	false
+// 1 =>	true
+// "" => false
+// "hello" =>true
+// null	=>false
+// undefined => false
+// NaN => false
+// [] => true
+// {} => true
+
+// string conversion
+
+let anyNumber=25
+
+let stringValue=String(anyNumber)
+
+console.log(typeof stringValue);
+console.log(stringValue);
+
+// 25 => "25"
+// true => "true"
+// false => "false"
+// null => "null"
+// undefined => "undefined"
+
+
+
+
+
